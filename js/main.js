4fa2b3d765ba9b8b12f88f7026252d0c9d977f3b
Stephen Hazleton <shazleto@gmail.com>
2016-02-25 00:27:50 +1300

    Maxing css better in widescreen (less sidebar, more content

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').animate({'max-width': '350px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
 
